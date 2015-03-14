b9e811d9132f3fe992bcaf309c23f4c22475e516
andrewpsuedonym <j.m.walker@live.co.uk>
2015-03-14 13:47:05 +0000

    Users working in populate script

--- mi4/populate_mi4.py
+++ mi4/populate_mi4.py
@@ -1,23 +1,37 @@
 import csv, sqlite3, sys
-
-mode =  sys.argv[1]
-print mode
 conn = sqlite3.connect('db.sqlite3')
 c = conn.cursor()
 
-if mode == "setUp":
-	
-	with open ('tables.sql') as f:
-		schema = f.read()
-	c.execute(schema)
-	conn.commit()
-	csv_reader = csv.reader(open('data.txt'))
-	to_db = tuple([i.decode('utf-8') for i in line] for line in csv_reader)
-	to_db
-	print "database set up"
+# Run with setUp or tearDown as arguments to set up or teardown DB
+# Run without arguements to print database
 
-if mode == "tearDown":
-	c.execute("DROP TABLE users")
-	conn.commit()
-	print "database cleared"
-	
+
+if len(sys.argv)==2:
+    mode = sys.argv[1]
+
+
+    if mode == "setUp":
+
+        with open ('tables.sql') as f:
+            schema = f.read()
+        c.executescript(schema)
+        conn.commit()
+        csv_reader = csv.reader(open('data.txt'))
+        to_db = tuple([i.decode('utf-8') for i in line] for line in csv_reader)
+        to_db
+        c.executemany("INSERT INTO users VALUES (?,?,?,?);", to_db)
+        
+
+
+        print "\ndatabase is set up\n"
+        print c.execute("SELECT * FROM users").fetchall()
+
+    if mode == "tearDown":
+        c.executescript("DROP TABLE users; DROP TABLE messages; DROP TABLE clients;")
+        conn.commit()
+        print "database cleared"
+
+else:
+	print c.execute("SELECT * FROM users;").fetchall()
+    print c.execute("SELECT * FROM messages;").fetchall()
+    print c.execute("SELECT * FROM clients;").fetchall()
--- mi4/tables.sql
+++ mi4/tables.sql
@@ -1,6 +1,19 @@
 CREATE TABLE users (
-    User text,
-    bio text,
-    id INTEGER PRIMARY KEY AUTOINCREMENT
-    
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username text,
+    password text,
+    bio text
+);
+
+CREATE TABLE messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    fromID INTEGER,
+    toID INTEGER
+    message text
+);
+
+CREATE TABLE clients (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name text,
+    data text
 );
