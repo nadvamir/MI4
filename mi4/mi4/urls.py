603bc0c1321128949fe4b2bb2c580daaeb16fdc0
Maksim Solovjov <himaxx2@gmail.com>
2015-03-14 14:10:53 +0000

    Add default views

--- mi4/mi4/urls.py
+++ mi4/mi4/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponseRedirect
 from django.contrib import admin
+from mi4_project import urls
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mi4.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('registration.backends.simple.urls'))
+    url(r'^mi4/', include(urls)),
+    url(r'^$', lambda r: HttpResponseRedirect('mi4/login')),
 )
--- mi4/mi4_project/views.py
+++ mi4/mi4_project/views.py
@@ -1,3 +1,30 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return HttpResponse('text')
+
+# main screen: has messages, agents, users, and message box
+def dashboard(request):
+    pass
+
+# client info screen
+def client(request):
+    pass
+
+# agent bio screen
+def bio(request):
+    pass
+
+# recipient page for sending the message
+def message(request):
+    pass
+
+# recipient page for updating bio
+def updBio(request):
+    pass
+
+# recipient page for updating data on clients
+def updData(request):
+    pass
