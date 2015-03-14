c965706771ce56089d1e3480715e0a1305b5dadc
andrewpsuedonym <j.m.walker@live.co.uk>
2015-03-14 11:24:27 +0000

    ignore edited

--- mi4/mi4/settings.py
+++ mi4/mi4/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mi4_project',
+    'registration'
 )
 
 MIDDLEWARE_CLASSES = (
