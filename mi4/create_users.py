926da724c2a0def350b3fd6d3d63e40091371cab
Maksim Solovjov <himaxx2@gmail.com>
2015-03-14 20:06:09 +0000

    Change passwords for default users

--- mi4/create_users.py
+++ mi4/create_users.py
@@ -4,17 +4,17 @@
 # before running other db scripts
 from django.contrib.auth.models import User
 
-user = User.objects.create_user('Jim', 'jim@jim.jim', 'Jim')
+user = User.objects.create_user('Jim', 'jim@jim.jim', 'JimHardPass9')
 user.first_name = 'Jim'
 user.second_name = 'Surname'
 user.save()
 
-user = User.objects.create_user('Maksim', 'maksim@jim.jim', 'Maksim')
+user = User.objects.create_user('Maksim', 'maksim@jim.jim', 'MaksimHardPass9')
 user.first_name = 'Maksim'
 user.second_name = 'Surname'
 user.save()
 
-user = User.objects.create_user('Fred', 'fred@jim.jim', 'Maksim')
+user = User.objects.create_user('Fred', 'fred@jim.jim', 'FredHardPass9')
 user.first_name = 'Fred'
 user.second_name = 'Surname'
 user.save()
